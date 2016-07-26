3a04372314833c8779de432eb7638eef004b78ff
j842 <j@842.be>
2016-07-26 22:00:08 +1200

    Simplify lua

--- drunner/service.lua
+++ drunner/service.lua
@@ -4,9 +4,9 @@ function drunner_setup()
 -- addconfig(NAME, DESCRIPTION, DEFAULT VALUE, TYPE, REQUIRED)
    addconfig("SECRETWORD","The secret word to use","squirrel","string",true)
 
--- addvolume(NAME, BACKUP, EXTERNAL)   
+-- addvolume(NAME, BACKUP, EXTERNAL)
 
--- addcontainer(NAME)   
+-- addcontainer(NAME)
    addcontainer("drunner/helloworld")  -- First one must always be this container.
 end
 
@@ -15,7 +15,7 @@ end
 -- e.g. helloworld run
 
 function run()
-  result=drun(dsub("docker run --rm -e SECRETWORD=${SECRETWORD} drunner/helloworld helloworld ${SERVICENAME} "))
+  result=drun("docker run --rm -e SECRETWORD=${SECRETWORD} drunner/helloworld helloworld ${SERVICENAME} ")
   if result~=0 then
      print("Failed to run helloworld.")
    end
@@ -26,7 +26,7 @@ function stop()
 end
 
 function help()
-   return dsub([[
+   return [[
    NAME
       ${SERVICENAME}
 
@@ -42,5 +42,5 @@ function help()
 
    DESCRIPTION
       A simple dRunner example. Built from ${IMAGENAME}.
-   ]])
+   ]]
 end
