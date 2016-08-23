b9c4ce3590d9bb0cf2cdb1bdf9da0af5690c0890
j842 <j@842.be>
2016-08-23 19:58:31 +1200

    Fix imagename

--- drunner/service.lua
+++ drunner/service.lua
@@ -2,20 +2,15 @@
 
 function drunner_setup()
 -- addconfig(NAME, DESCRIPTION, DEFAULT VALUE, TYPE, REQUIRED)
-   addconfig("SECRETWORD","The secret word to use","squirrel","string",true)
-
 -- addvolume(NAME, [BACKUP], [EXTERNAL])
-
 -- addcontainer(NAME)
-   addcontainer("drunner/helloworld")  -- First one must always be this container.
+   addconfig("SECRETWORD","The secret word to use","squirrel","string",true)
 end
 
-
--- everything past here are functions that can be run from the commandline,
--- e.g. helloworld run
-
+-- functions that can be run from the commandline, e.g. helloworld run
 function run()
-  result=drun("docker","run","--rm", "-e", "SECRETWORD=${SECRETWORD}", "drunner/helloworld", "helloworld", "${SERVICENAME}", "${IMAGENAME}")
+  result=drun("docker","run","--rm", "-e", "SECRETWORD=${SECRETWORD}", "${IMAGENAME}",
+               "helloworld", "${SERVICENAME}", "${IMAGENAME}")
   if result~=0 then
      print("Failed to run helloworld.")
    end
